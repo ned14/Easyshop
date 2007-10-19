545e88067dd88c494d9e6fd1e789b8d7f1041d87
kai.diefenbach <kai.diefenbach@localhost>
2007-10-19 18:06:38 +0000

    Removed old orders container.
    svn path=/EasyShop/trunk/; revision=66

--- EasyShop/content/shop/EasyShopContainers.py
+++ EasyShop/content/shop/EasyShopContainers.py
@@ -3,23 +3,15 @@ from zope.interface import implements
 
 # Archetypes imports
 from Products.Archetypes.atapi import OrderedBaseFolder
-from Products.Archetypes.atapi import BaseBTreeFolder
 from Products.Archetypes.atapi import registerType
 
 # EasyShop imports
 from Products.EasyShop.config import PROJECTNAME
 from Products.EasyShop.interfaces import IGroupsContainer
-from Products.EasyShop.interfaces import IOrdersContainer
 
 class EasyShopGroups(OrderedBaseFolder):
     """A simple container to hold groups.
     """    
     implements(IGroupsContainer)
 
-class Orders(BaseBTreeFolder):
-    """A simple container to hold orders.
-    """
-    implements(IOrdersContainer)
-                 
 registerType(EasyShopGroups, PROJECTNAME)
-registerType(Orders, PROJECTNAME)
