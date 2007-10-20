1720a093079721c04e8336040d54b2fb6cd41b35
kai.diefenbach <kai.diefenbach@localhost>
2007-10-20 14:33:27 +0000

    Factored out from EasyShop core.
    svn path=/easyshop.groups/trunk/; revision=92

--- easyshop.groups/easyshop/groups/content/containers.py
+++ easyshop.groups/easyshop/groups/content/containers.py
@@ -9,9 +9,9 @@ from Products.Archetypes.atapi import registerType
 from Products.EasyShop.config import PROJECTNAME
 from Products.EasyShop.interfaces import IGroupsContainer
 
-class GroupsContainers(OrderedBaseFolder):
+class GroupsContainer(OrderedBaseFolder):
     """A simple container to hold groups.
     """    
     implements(IGroupsContainer)
 
-registerType(GroupsContainers, PROJECTNAME)
+registerType(GroupsContainer, PROJECTNAME)
