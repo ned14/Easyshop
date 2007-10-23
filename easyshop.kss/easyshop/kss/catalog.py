96f01c69515d023b07515ad29a64f689656b04cb
kai.diefenbach <kai.diefenbach@localhost>
2007-10-23 15:17:09 +0000

    Added KSS for products_view.
    svn path=/easyshop.kss/trunk/; revision=133

--- easyshop.kss/easyshop/kss/catalog.py
+++ easyshop.kss/easyshop/kss/catalog.py
@@ -1,3 +1,6 @@
+# python imports
+import re
+
 # kss imports
 from plone.app.kss.plonekssview import PloneKSSView
 from kss.core import kssaction
@@ -8,6 +11,72 @@ from Products.CMFCore.utils import getToolByName
 class CatalogKSSView(PloneKSSView):
     """
     """
+    @kssaction    
+    def showProducts(self, letter=None, form={}):
+        """
+        """
+        catalog = getToolByName(self.context, "portal_catalog")
+        kss_core  = self.getCommandSet("core")
+                
+        searchable_text = form.get("searchable_text", "")
+
+        products = []        
+        if searchable_text != "":
+            products = catalog.searchResults(
+                path = "/".join(self.context.getPhysicalPath()),
+                portal_type = "Product",
+                SearchableText = searchable_text,
+                sort_on = "sortable_title",
+            )
+
+            # Workaround for browser message: "you have already sent this form"
+            form = """
+                <form id="search-products-form"
+                      action="."
+                      method="post"
+                      style="display:inline">
+                    <input type="text"
+                           name="searchable_text"
+                           value="%s" />
+                    <input id="search-products"
+                           type="submit"
+                           value="OK" />
+                </form>""" % searchable_text
+            kss_core.replaceHTML("#search-products-form", form)
+            
+        else:
+            if letter == "0-9":
+                brains = catalog.searchResults(
+                    path = "/".join(self.context.getPhysicalPath()),
+                    portal_type = "Product",
+                    sort_on = "sortable_title",
+                )
+                    
+                for brain in brains:
+                    if re.match("\d", brain.Title):
+                        products.append(brain)
+            else:
+                brains = catalog.searchResults(
+                    path = "/".join(self.context.getPhysicalPath()),
+                    portal_type = "Product",
+                    Title = "%s*" % letter,
+                    sort_on = "sortable_title",
+                )
+            
+                for brain in brains:
+                    if brain.Title.upper().startswith(letter):
+                        products.append(brain)
+        
+        html = ""
+        for product in products:
+            html += """<div>"""
+            html += """<a href="." class="product-details kssattr-uid-%s">[Details]</a> """ % product.UID
+            html += """<a href="%s">%s</a>""" % (product.getURL(), product.Title)
+            html += """</div>"""
+            
+        kss_core.replaceInnerHTML('#products', html)    
+        kss_core.replaceInnerHTML('#product-details-box', "")
+                        
     @kssaction    
     def showProductDetails(self, uid):
         """
