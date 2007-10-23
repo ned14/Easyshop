1ffa155cc5a98bb98e3856d607748331fc344cee
kai.diefenbach <kai.diefenbach@localhost>
2007-10-23 11:59:19 +0000

    Cleaned up profile.
    svn path=/EasyShop/trunk/; revision=124

--- EasyShop/browser/admin/test_environment.py
+++ EasyShop/browser/admin/test_environment.py
@@ -1,6 +1,5 @@
-# zope imports
-from zope.interface import Interface
-from zope.interface import implements
+# Python imports
+import random
 
 # Five imports
 from Products.Five.browser import BrowserView
@@ -8,14 +7,66 @@ from Products.Five.browser import BrowserView
 # CMFCore imports
 from Products.CMFCore.utils import getToolByName
 
-# EasyShop imports
-from Products.EasyShop.tests.utils import createTestEnvironment
+LETTERS = [chr(i) for i in range(65, 91)]
 
 class TestEnvironmentView(BrowserView):
     """
     """
-    def create(self):
+    def createProducts1(self):
+        """
+        """                            
+        shop = self.context
+
+        categories = []
+        for i in range(1, 21):
+            id = "category-%s" % i
+            shop.categories.manage_addProduct["EasyShop"].addCategory(id, title="Category %s" %i)
+            
+            category = shop.categories.get(id)
+            categories.append(category)
+            
+            wftool = getToolByName(self.context, "portal_workflow")
+            wftool.doActionFor(category, "publish")
+
+        for i in range(1, 101):
+            title = self.createTitle()
+            id = title.lower()
+            shop.products.manage_addProduct["EasyShop"].addProduct(id, title=title)
+            product = shop.products.get(id)            
+
+            # add category
+            category = random.choice(categories)
+            category.addReference(product, "easyshopcategory_easyshopproduct")
+            
+            wftool.doActionFor(product, "publish")
+            
+        self.context.portal_catalog.manage_catalogRebuild()
+        
+    def createProducts2(self):
+        """Add all products to one category.
+        """                            
+        shop = self.context
+        
+        
+        id = "category"
+        shop.categories.manage_addProduct["EasyShop"].addCategory(id, title="Category")        
+        category = shop.categories.get(id)
+        
+        wftool = getToolByName(self.context, "portal_workflow")
+        wftool.doActionFor(category, "publish")
+
+        for i in range(1, 101):
+            title = self.createTitle()
+            id = title.lower()
+            shop.products.manage_addProduct["EasyShop"].addProduct(id, title=title)
+            product = shop.products.get(id)            
+
+            category.addReference(product, "easyshopcategory_easyshopproduct")            
+            wftool.doActionFor(product, "publish")
+            
+        self.context.portal_catalog.manage_catalogRebuild()
+        
+    def createTitle(self):
         """
         """
-        portal = self.context
-        createTestEnvironment(portal)
+        return "".join([random.choice(LETTERS) for i in range(1, 10)])
