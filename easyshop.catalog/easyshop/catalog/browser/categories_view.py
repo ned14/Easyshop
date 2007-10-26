7b9c6242355a1584727cf12373bfa35e4225e83f
kai.diefenbach <kai.diefenbach@localhost>
2007-10-26 07:43:44 +0000

    Introducing new categories view
    svn path=/easyshop.catalog/trunk/; revision=209

---
+++ easyshop.catalog/easyshop/catalog/browser/categories_view.py
@@ -0,0 +1,11 @@
+# Five imports
+from Products.Five.browser import BrowserView
+
+# easyshop imports
+from easyshop.core.interfaces import ICategoryManagement
+from easyshop.core.interfaces import IShopManagement
+
+class CategoriesView(BrowserView):
+    """
+    """
+    pass
