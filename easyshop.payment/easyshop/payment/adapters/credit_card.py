8b92d79c6d0dd46f47cf413fedf5a70ba957369c
kai.diefenbach <kai.diefenbach@localhost>
2007-11-18 17:56:58 +0000

    Anonmyous checkout, credit card handling, cleanups
    svn path=/easyshop.payment/branches/cc/; revision=296

--- easyshop.payment/easyshop/payment/adapters/credit_card.py
+++ easyshop.payment/easyshop/payment/adapters/credit_card.py
@@ -5,8 +5,15 @@ from zope.component import adapts
 # easyshop imports
 from easyshop.core.interfaces import ICompleteness
 from easyshop.core.interfaces import ICreditCard
+from easyshop.core.interfaces import IItemManagement
+from easyshop.core.interfaces import IPaymentManagement
 from easyshop.core.interfaces import IPaymentProcessing
+from easyshop.core.interfaces import IPrices
+
 from easyshop.core.interfaces import IType
+
+# zc.authorizedotnet import 
+from zc.authorizedotnet.processing import CcProcessor
 
 class CreditCardType:
     """Provides IType for direct debit content objects.
@@ -25,8 +32,8 @@ class CreditCardType:
         return "credit-card"
         
 
-class CreditCardPaymentProcessor:
-    """Provides IPaymentProcessing for direct debit content objects.
+class AuthorizeNetCreditCardPaymentProcessor:
+    """Provides IPaymentProcessing for credit cards content objects.
     """
     implements(IPaymentProcessing)
     adapts(ICreditCard)
@@ -39,7 +46,44 @@ class CreditCardPaymentProcessor:
     def process(self, order=None):
         """
         """
+        credit_card = IPaymentManagement(order).getSelectedPaymentMethod()
+        card_num = credit_card.card_number
+        exp_date = credit_card.card_expiration_date
+
+        line_items = []
+        for i, item in enumerate(IItemManagement(order).getItems()):
+            if item.getProductTax() > 0:
+                tax = "Y"
+            else:
+                tax = "N"
+            
+            line_items.append((
+                i+1,
+                item.getProduct().Title(),
+                item.getProductQuantity(),
+                item.getProductPriceGross(),
+                tax,                
+            ))
+            
+        amount = IPrices(order).getPriceForCustomer()
+
         return "PAYED"
+        
+        cc = CcProcessor(server=SERVER_NAME, login=LOGIN, key=KEY)
+
+        result = cc.authorize(
+            amount     = amount,
+            card_num   = card_num,
+            exp_date   = exp_date,
+            line_items = line_items)
+        
+        if result == "approved":
+            result = cc.captureAuthorized(trans_id=result.trans_id)
+        
+        if result.response == "approved":
+            return "PAYED"
+        else:
+            return "NOT_PAYED"
         
 class CreditCardCompleteness:
     """Provides ICompleteness for direct debit content objects.
