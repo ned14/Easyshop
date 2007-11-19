074c6124ad8d4d0526fc41fd859aac255ecb9eaa
kai.diefenbach <kai.diefenbach@localhost>
2007-11-19 05:21:27 +0000

    First working version of selectable prices (gross or net).
    svn path=/easyshop.catalog/branches/tax/; revision=303

--- easyshop.catalog/easyshop/catalog/adapters/product/price.py
+++ easyshop.catalog/easyshop/catalog/adapters/product/price.py
@@ -2,12 +2,10 @@
 from zope.interface import implements
 from zope.component import adapts
 
-# CMFCore imports
-from Products.CMFCore.utils import getToolByName
-
 # easyshop imports
 from easyshop.core.interfaces import IPrices
 from easyshop.core.interfaces import IProduct
+from easyshop.core.interfaces import IShopManagement
 from easyshop.core.interfaces import ITaxes
 
 class ProductPriceCalculator:
@@ -20,32 +18,34 @@ class ProductPriceCalculator:
         """
         """
         self.context = context
-
+        self.gross_prices = IShopManagement(context).getShop().getGrossPrices()
+        self.tax_abs = ITaxes(self.context).getTaxForCustomer()
+        
     def getPriceForCustomer(self):
         """Returns the end price of the customer. It is the net price plus
         the tax for the customer. By default (or in most cases) this should 
         be the same price as price gross (as an customer will get the standard
         taxes of the country the shop is running).
-        """
+        """        
         tax_abs_customer = ITaxes(self.context).getTaxForCustomer()
-        price = self.getPriceNet() + tax_abs_customer
-
-        return price
+        return self.getPriceNet() + tax_abs_customer
 
     def getPriceNet(self):
         """Returns the net price of the product. It is just the difference of 
         price gross and the default tax (which mean the standard tax for the country
         in which the shop is running) for the product. 
         """
-        tax_abs = ITaxes(self.context).getTax()
-        price = self.context.getPriceGross() - tax_abs
-
-        return price
+        if self.gross_prices == True:
+            return self.context.getPriceGross() - self.tax_abs
+        else:
+            return self.context.getPriceGross()
 
     def getPriceGross(self):
         """Returns the gross price of the product. This is entered by the 
         customer and the base for all other prices (price net, price for 
         customer but also for cart, cart item, order and order item)
         """
-        # just to make the adapter complete
-        return self.context.getPriceGross()
+        if self.gross_prices == True:
+            return self.context.getPriceGross()
+        else:
+            return self.context.getPriceGross() + self.tax_abs
