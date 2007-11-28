6355ce6782853083479a8ac82bedbe4cf7ff7b10
kai.diefenbach <kai.diefenbach@localhost>
2007-11-28 17:27:08 +0000

    Using new formats infrastructure
    svn path=/easyshop.kss/trunk/; revision=334

--- easyshop.kss/easyshop/kss/formatter.py
+++ easyshop.kss/easyshop/kss/formatter.py
@@ -19,24 +19,8 @@ class FormatterKSSView(PloneKSSView):
     def saveFormatter(self, form, portlethash):
         """
         """
-        fi = IFormats(self.context)
-        f = fi.getFormatter()
-        
-        products_per_line = form.get("products_per_line", 0)
-        lines_per_page    = form.get("lines_per_page", 0)
-        image_size        = form.get("image_size", "mini")
-        text              = form.get("text", "")
-        product_height    = form.get("product_height", 0)
-        
-        products_per_line = int(products_per_line)
-        lines_per_page    = int(lines_per_page)
-        product_height    = int(product_height)
-                
-        f.setProductsPerLine(products_per_line)
-        f.setLinesPerPage(lines_per_page)
-        f.setImageSize(image_size)
-        f.setProductHeight(product_height)        
-        f.setText(text)
+        fi = IFormats(self.context)        
+        fi.setFormats(form)
         
         kss_core  = self.getCommandSet("core")
         kss_zope  = self.getCommandSet("zope")
