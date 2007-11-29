3e9ca7ce819378731c8d02bbfbcfc1982af10e39
kai.diefenbach <kai.diefenbach@localhost>
2007-11-29 18:55:29 +0000

    Fixed display.
    svn path=/easyshop.catalog/branches/tax/; revision=348

--- easyshop.catalog/easyshop/catalog/viewlets/product_selector.py
+++ easyshop.catalog/easyshop/catalog/viewlets/product_selector.py
@@ -97,7 +97,7 @@ class ProductSelectorViewlet(ViewletBase):
                 else:
                     text = ""
                 
-                if (index + 1) % products_per_line == 0 and products_per_line > 1:
+                if (index + 1) % products_per_line == 0:
                     klass = "last"
                 else:
                     klass = "notlast"
