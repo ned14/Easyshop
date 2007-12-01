bb61dbfca7526c6604875959872bde7f51de5330
kai.diefenbach <kai.diefenbach@localhost>
2007-12-01 20:26:28 +0000

    Reorganization of adapters
    svn path=/easyshop.payment/branches/cc/; revision=367

---
+++ easyshop.payment/easyshop/payment/adapters/payment_method_management.py
@@ -0,0 +1,80 @@
+# zope imports
+from zope.interface import implements
+from zope.component import adapts
+
+# CMFCore imports
+from Products.CMFCore.utils import getToolByName
+
+# easyshop imports
+from easyshop.core.interfaces import IPaymentMethodManagement
+from easyshop.core.interfaces import ISelectablePaymentMethod
+from easyshop.core.interfaces import IShop
+from easyshop.core.interfaces import IValidity
+
+class PaymentMethodManagement:
+    """An adapter which provides IPaymentMethodManagement for shop content
+    objects.
+    """
+    implements(IPaymentMethodManagement)
+    adapts(IShop)
+    
+    def __init__(self, context):
+        """
+        """
+        self.context = context
+        self.paymentmethods = self.context.paymentmethods
+
+    def deletePaymentMethod(self, id):
+        """
+        """
+        try:
+            self.context.paymentmethods.manage_delObjects(id)
+        except AttributeError:
+            return False
+
+        return True
+
+    def getPaymentMethod(self, id):
+        """Returns payment method by given id.
+        """
+        try:
+            return self.paymentmethods[id]
+        except KeyError:
+            return None
+
+    def getPaymentMethods(self, check_validity=False):
+        """Returns the payment methods on shop level. 
+        """
+        mtool = getToolByName(self.context, "portal_membership")
+            
+        result = []
+        for object in self.paymentmethods.objectValues():
+
+            if check_validity and \
+               IValidity(object).isValid(object) == False:
+                continue                    
+            
+            if mtool.checkPermission("View", object) is not None:
+                result.append(object)
+        
+        return result
+            
+    def getSelectablePaymentMethods(self, check_validity=False):
+        """Returns payment method which are selectable by a customer.
+        """
+        mtool = getToolByName(self.context, "portal_membership")
+            
+        result = []
+        for object in self.paymentmethods.objectValues():
+
+            if ISelectablePaymentMethod.providedBy(object) == False:
+                continue
+
+            if check_validity and\
+               IValidity(object).isValid(object) == False:
+                continue                    
+            
+            if mtool.checkPermission("View", object) is not None:
+                result.append(object)
+        
+        return result
