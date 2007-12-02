eccb3d8e1822026a15cabbb53fa078d3ebbf98e6
kai.diefenbach <kai.diefenbach@localhost>
2007-12-02 16:28:53 +0000

    Solved some todos.
    svn path=/easyshop.taxes/branches/tax/; revision=384

--- easyshop.taxes/easyshop/taxes/adapters/tax_management.py
+++ easyshop.taxes/easyshop/taxes/adapters/tax_management.py
@@ -2,15 +2,9 @@
 from zope.interface import implements
 from zope.component import adapts
 
-# CMFCore imports
-from Products.CMFCore.utils import getToolByName
-
 # easyshop imports
 from easyshop.core.interfaces import IShop
-from easyshop.core.interfaces import ITaxes
 from easyshop.core.interfaces import ITaxManagement
-from easyshop.core.interfaces import IValidity
-from easyshop.core.interfaces import IShopManagement
 
 class TaxManagement:
     """An adapter, which provides methods to manage tax objects for shop 
@@ -23,50 +17,22 @@ class TaxManagement:
         """
         """
         self.context = context
+        self.taxes = self.context.taxes
     
     def getCustomerTaxes(self):
         """
         """
-        return self.context.taxes.objectValues("CustomerTax")
-        
-        # Todo: Optimize        
-        catalog = getToolByName(self.context, "portal_catalog")
-        brains = catalog.searchResults(
-            path = "/".join(self.context.taxes.getPhysicalPath()),
-            portal_type="CustomerTax",
-            sort_on = "getObjPositionInParent",
-        )
-
-        # Todo
-        return [brain.getObject() for brain in brains]
+        return self.taxes.objectValues("CustomerTax")
 
     def getDefaultTaxes(self):
         """
         """
-        return self.context.taxes.objectValues("DefaultTax")
-        shop = IShopManagement(self.context).getShop()
-        
-        catalog = getToolByName(self.context, "portal_catalog")
-        brains = catalog.searchResults(
-            path = "/".join(self.context.taxes.getPhysicalPath()),
-            portal_type="DefaultTax",
-            sort_on = "getObjPositionInParent",
-        )
-
-        # Todo
-        return [brain.getObject() for brain in brains]
+        return self.taxes.objectValues("DefaultTax")
                     
     def getTax(self, id):
         """
         """
-        catalog = getToolByName(self.context, "portal_catalog")
-        brains = catalog.searchResults(
-            portal_type = ("DefaultTax", "CustomerTax"),
-            path = "/".join(self.context.taxes.getPhysicalPath()),
-            id = id,
-        )
-
         try:
-            return brains[0].getObject()
-        except IndexError:
+            return self.taxes[id]
+        except KeyError:
             return None
