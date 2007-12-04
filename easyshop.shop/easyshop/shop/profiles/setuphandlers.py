67e0790a10bd65cdae96554f168de188ce71d896
kai.diefenbach <kai.diefenbach@localhost>
2007-12-04 12:40:03 +0000

    Cleaned up setup script.
    svn path=/easyshop.shop/trunk/; revision=423

--- easyshop.shop/easyshop/shop/profiles/setuphandlers.py
+++ easyshop.shop/easyshop/shop/profiles/setuphandlers.py
@@ -15,52 +15,9 @@ def installDependencies(portal):
         if product in ids:
             qit.installProduct(product)
     
-def setupFormController(portal):
-    """
-    """
-    portal_form_controller = getToolByName(portal, "portal_form_controller")
-    portal_form_controller.addFormAction("validate_integrity",
-                                         "success",
-                                         "Customer",
-                                         "checkout",
-                                         "redirect_to",
-                                         "python: '%s/easyshoporder_preview' % object.getShop().absolute_url()")
-    
-    for klass in ("Address", "DirectDebit"):    
-        portal_form_controller.addFormAction("validate_integrity",
-                                             "success",
-                                             klass,
-                                             "",
-                                             "traverse_to",
-                                             "string:easyshop_goto")
-
-        portal_form_controller.addFormAction("base_edit",
-                                             "success",
-                                             klass,
-                                             "cancel",
-                                             "traverse_to",
-                                             "string:easyshop_goto")
-
-    portal_form_controller.addFormAction("validate_integrity",
-                                         "success",
-                                         "Address",
-                                         "",
-                                         "traverse_to",
-                                         "string:easyshop_goto")
-
-
-    portal_form_controller.addFormAction("validate_integrity",
-                                         "success",
-                                         "Customer",
-                                         "",
-                                         "redirect_to",
-                                         "python: '%s?fieldset=%s' % (object.REQUEST.URL, object.REQUEST.form.get('fieldset'))")
-                                         
 def importVarious(context):
     """Import various settings.
     """
     portal = context.getSite()
-
-    # setupFormController(portal)
     installDependencies(portal)
 
