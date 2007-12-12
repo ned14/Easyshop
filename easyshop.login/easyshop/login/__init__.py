aab479dc5a2f332213cc0873e8eb50619b76a086
kai.diefenbach <kai.diefenbach@localhost>
2007-12-12 10:51:45 +0000

    Created branch for Plone 2.5
    svn path=/easyshop.login/branches/2.5/; revision=516

---
+++ easyshop.login/easyshop/login/__init__.py
@@ -0,0 +1,7 @@
+from Products.CMFCore import DirectoryView
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+    
+    # Register skin directory
+    DirectoryView.registerDirectory('skins', globals())
