f1fd61ec2650d0e5a4c56cb8f4ae0062059e06f7
kai.diefenbach <kai.diefenbach@localhost>
2007-12-15 17:06:20 +0000

    Renamed
    svn path=/easyshop.stocks/branches/linguaplone/; revision=576

---
+++ easyshop.stocks/easyshop/stocks/adapters/stock_management.py
@@ -0,0 +1,48 @@
+# zope imports
+from zope.component import adapts
+from zope.interface import implements
+
+# easyshop imports
+from easyshop.core.interfaces import IData
+from easyshop.core.interfaces import IItemManagement
+from easyshop.core.interfaces import IShop
+from easyshop.core.interfaces import IStockManagement
+from easyshop.core.interfaces import IValidity
+
+class StockManagement:
+    """Adapter which provides IStockManagement for shop content objects.
+    """
+    implements(IStockManagement)
+    adapts(IShop)
+    
+    def __init__(self, context):
+        """
+        """
+        self.context = context
+        self.stock_information = context["stock-information"]
+
+    def getStockInformationFor(self, product):
+        """
+        """
+        for information in self.stock_information.objectValues():
+            if IValidity(information).isValid(product) == True:
+                return information
+        
+        return None
+        
+    def getStockInformations(self):
+        """
+        """
+        return self.stock_information.objectValues()
+        
+    def removeCart(self, cart):
+        """
+        """        
+        for cart_item in IItemManagement(cart).getItems():
+            product = cart_item.getProduct()
+            
+            # Remove only when product has not unlimited amount
+            if product.getUnlimitedAmount() == False:
+                amount = cart_item.getAmount()
+                new_amount = product.getStockAmount() - amount
+                product.setStockAmount(new_amount)
