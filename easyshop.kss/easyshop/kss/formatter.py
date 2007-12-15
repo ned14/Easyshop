93ab8b1f2ad6f354e1d6da2a4de834ad56db9108
kai.diefenbach <kai.diefenbach@localhost>
2007-12-15 14:49:43 +0000

    "test"
    svn path=/easyshop.kss/branches/linguaplone/; revision=562

---
+++ easyshop.kss/easyshop/kss/formatter.py
@@ -0,0 +1,43 @@
+# zope imports
+from zope.component.exceptions import ComponentLookupError
+
+# kss imports
+from plone.app.kss.plonekssview import PloneKSSView
+from kss.core import kssaction
+
+# easyshop imports
+from easyshop.core.interfaces import IFormats
+
+class FormatterKSSView(PloneKSSView):
+    """
+    """
+    @kssaction
+    def saveFormatter(self, form, portlethash):
+        """
+        """
+        fi = IFormats(self.context)        
+        fi.setFormats(form)
+        
+        kss_core  = self.getCommandSet("core")
+        kss_zope  = self.getCommandSet("zope")
+        kss_plone = self.getCommandSet("plone")
+        
+        layout = form.get("layout")
+
+        if layout == "categories-view":
+            kss_zope.refreshViewlet(kss_core.getHtmlIdSelector("categories-list"),
+                                    manager="easyshop.categories-manager",
+                                    name="easyshop.categories-viewlet")
+
+        elif layout == "products-view":
+            kss_zope.refreshViewlet(kss_core.getHtmlIdSelector("products-list"),
+                                    manager="easyshop.products-manager",
+                                    name="easyshop.products-viewlet")
+
+        elif layout == "product-selector-view":
+            kss_zope.refreshViewlet(kss_core.getHtmlIdSelector("products-list"),
+                                    manager="easyshop.product-selector-manager",
+                                    name="easyshop.product-selector-viewlet")                                    
+
+        kss_plone.refreshPortlet(portlethash)                                    
+        
