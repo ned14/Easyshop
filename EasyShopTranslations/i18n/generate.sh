af7802fa35d66dc8e7f031b0783c47272c4fd763
kai.diefenbach <kai.diefenbach@localhost>
2007-12-20 12:40:04 +0000

    Updated to new structure.
    svn path=/EasyShopTranslations/trunk/; revision=614

--- EasyShopTranslations/i18n/generate.sh
+++ EasyShopTranslations/i18n/generate.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 i18ndude rebuild-pot --pot easyshop.pot --create EasyShop --merge extra.pot\
-    ../skins/easyshop_checkout\
-    ../skins/easyshop_macros\
-    ../skins/easyshop_mail_templates\
-    ../skins/easyshop_manage\
-    ../skins/easyshop_portlets\
-    ../skins/easyshop_templates\
-    ../browser
-
+    ../../../src/easyshop.carts\
+    ../../../src/easyshop.catalog\
+    ../../../src/easyshop.checkout\
+    ../../../src/easyshop.core\
+    ../../../src/easyshop.customers\
+    ../../../src/easyshop.groups\
+    ../../../src/easyshop.information\
+    ../../../src/easyshop.io\
+    ../../../src/easyshop.order\
+    ../../../src/easyshop.payment\
+    ../../../src/easyshop.shipping\
+    ../../../src/easyshop.shop\
+    ../../../src/easyshop.stocks\
+                                    
 i18ndude sync --pot easyshop.pot easyshop-de.po
 i18ndude sync --pot easyshop-schema.pot easyshop-schema-de.po
 i18ndude sync --pot easyshop-plone.pot easyshop-plone-de.po
