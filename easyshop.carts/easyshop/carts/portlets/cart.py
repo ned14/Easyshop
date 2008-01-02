3bd1746d85e9fffbaebe8ad05dcf13e9d06cbf0c
kai.diefenbach <kai.diefenbach@localhost>
2008-01-02 10:10:57 +0000

    svn path=/easyshop.carts/branches/2.5/; revision=703

--- easyshop.carts/easyshop/carts/portlets/cart.py
+++ easyshop.carts/easyshop/carts/portlets/cart.py
@@ -1,51 +1,98 @@
 # zope imports
-from zope.i18nmessageid import MessageFactory
 from zope.interface import implements
-
-# plone imports
-from plone.app.portlets.portlets import base
-from plone.portlets.interfaces import IPortletDataProvider
+from zope.interface import Interface
 
 # Five imports
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser import BrowserView
 
-# create message factory
-_ = MessageFactory("EasyShop")
+# plone imports
+from plone.memoize.instance import memoize
 
-class ICartPortlet(IPortletDataProvider):
+# easyshop imports
+from easyshop.core.interfaces import ICartManagement
+from easyshop.core.interfaces import ICurrencyManagement
+from easyshop.core.interfaces import IPrices
+from easyshop.core.interfaces import IItemManagement
+from easyshop.core.interfaces import IShopManagement
+
+class ICartPortletView(Interface):
     """
     """
-
-class Assignment(base.Assignment):
-    """
-    """
-    implements(ICartPortlet)
-
-    def __init__(self):
+    def getAmountOfArticles():
         """
         """
-
-    @property
-    def title(self):
+                        
+    def getCartPrice():
         """
         """
-        return _(u"Cart")
-
-class Renderer(base.Renderer):
-    """
-    """
-    render = ViewPageTemplateFile('cart.pt')
-
-    @property
-    def available(self):
+                        
+    def getShopUrl():
         """
         """
-        return True
         
-class AddForm(base.NullAddForm):
-    """
-    """
-    def create(self):
+    def showCheckOutLink():
         """
         """
-        return Assignment()
+
+class CartPortletView(BrowserView):
+    """
+    """
+    implements(ICartPortletView)
+    
+    def getAmountOfArticles(self):
+        """
+        """
+        cart = self._getCart()
+        if cart is None:
+            return 0
+            
+        amount = 0
+        for item in IItemManagement(cart).getItems():
+            amount += item.getAmount()
+        
+        return amount
+                        
+    def getCartPrice(self):
+        """
+        """
+        shop = self._getShop()
+        cm = ICurrencyManagement(shop)
+        
+        if ICartManagement(shop).hasCart():
+            cart = self._getCart()
+            price = IPrices(cart).getPriceForCustomer()
+            return cm.priceToString(price)
+        else:
+            return cm.priceToString(0.0)
+                        
+    def getShopUrl(self):
+        """
+        """
+        shop = self._getShop()
+        return shop.absolute_url()
+        
+    def showCheckOutLink(self):
+        """
+        """
+        cart = self._getCart()
+        
+        if cart is None:
+            return False
+            
+        if IItemManagement(cart).hasItems() == False:
+            return False
+        
+        return True
+    
+    @memoize
+    def _getShop(self):
+        """
+        """
+        return IShopManagement(self.context).getShop()
+
+    @memoize
+    def _getCart(self):
+        """
+        """
+        shop = self._getShop()
+        return ICartManagement(shop).getCart()
