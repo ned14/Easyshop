d5ce7605ed741dabefbc0b40446e371a8de70cbd
kai.diefenbach <kai.diefenbach@localhost>
2008-01-02 12:19:54 +0000

    Removed schema from BankAccount.
    svn path=/easyshop.payment/branches/2.5/; revision=714

--- easyshop.payment/easyshop/payment/content/bank_account.py
+++ easyshop.payment/easyshop/payment/content/bank_account.py
@@ -8,57 +8,11 @@ from Products.Archetypes.atapi import *
 from easyshop.core.config import *
 from easyshop.core.interfaces import IBankAccount
 
-schema = Schema((
-
-    StringField(
-        name='account_number',
-        required=1,
-        widget=StringWidget(
-            label='Account Number',
-            label_msgid='schema_account_number_label',
-            i18n_domain='EasyShop',
-        )
-    ),
-
-    StringField(
-        name='bank_identification_code',
-        required=1,        
-        widget=StringWidget(
-            label='Bank Identification Code',
-            label_msgid='schema_bank_identification_code_label',
-            i18n_domain='EasyShop',
-        )
-    ),
-
-    StringField(
-        name='depositor',
-        required=1,        
-        widget=StringWidget(
-            label='Depositor',
-            label_msgid='schema_name_label',
-            i18n_domain='EasyShop',
-        )
-    ),
-
-    StringField(
-        name='bank_name',
-        required=1,
-        widget=StringWidget(
-            label='Bankname',
-            label_msgid='schema_bankname_label',
-            i18n_domain='EasyShop',
-        )
-    ),
-
-),
-)
-
 class BankAccount(BaseContent):
     """Holds all relevant informations for direct debit payment method. This is 
     a bank account.
     """    
     implements(IBankAccount)
-    schema = schema
     
     def Title(self):
         """
