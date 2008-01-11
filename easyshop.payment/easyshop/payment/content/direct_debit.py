793b2d6b7d0851117080dd4da682f13e1b542feb
kai.diefenbach <kai.diefenbach@localhost>
2008-01-11 13:28:58 +0000

    Merged -r624:752 from trunk.
    svn path=/easyshop.payment/branches/linguaplone/; revision=753

--- easyshop.payment/easyshop/payment/content/direct_debit.py
+++ easyshop.payment/easyshop/payment/content/direct_debit.py
@@ -2,9 +2,7 @@
 from AccessControl import ClassSecurityInfo
 
 # zope imports
-from zope.component.factory import Factory
 from zope.interface import implements
-from zope.schema.fieldproperty import FieldProperty
 
 # Archetypes imports
 from Products.Archetypes.atapi import OrderedBaseFolder
@@ -13,12 +11,7 @@ from Products.Archetypes.atapi import registerType
 # ATContentTypes imports
 from Products.ATContentTypes.content.base import ATCTMixin
 
-# plone imports
-from plone.app.content.item import Item
-
 # easyshop imports
-from easyshop.core.config import _
-from easyshop.core.interfaces import ICreditCard
 from easyshop.core.interfaces import IDirectDebitPaymentMethod
 
 # easyshop imports
