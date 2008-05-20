ecd4de408cd09113c207769c6e4f6e7134ca1a27
kai.diefenbach <kai.diefenbach@localhost>
2008-05-20 13:23:26 +0000

    Added image and note to shipping method content object.
    svn path=/easyshop.shipping/trunk/; revision=1085

--- easyshop.shipping/easyshop/shipping/content/shipping_method.py
+++ easyshop.shipping/easyshop/shipping/content/shipping_method.py
@@ -2,15 +2,48 @@
 from zope.interface import implements
 
 # Archetypes imports
-from Products.Archetypes.atapi import OrderedBaseFolder
-from Products.Archetypes.atapi import registerType
+from Products.Archetypes.atapi import *
+
+# ATContentTypes imports
+from Products.ATContentTypes.content.base import ATCTMixin
 
 # easyshop imports
 from easyshop.core.config import PROJECTNAME
 from easyshop.core.interfaces import IShippingMethod
 
-schema = OrderedBaseFolder.schema.copy()
-schema["description"].schemata = "default"
+schema = Schema((
+    TextField(
+        name = "note",
+        widget=TextAreaWidget(
+            label = "Note",
+            description = "This text will be displayed on the invoice",
+            label_msgid = "schema_note_label",
+            description_msgid = "schema_note_description",
+            i18n_domain = "EasyShop"),
+    ),
+    
+    ImageField(
+        name='image',
+        sizes= {'large'   : (768, 768),
+                'preview' : (400, 400),
+                'mini'    : (200, 200),
+                'thumb'   : (128, 128),
+                'tile'    :  (64, 64),
+                'icon'    :  (32, 32),
+                'listing' :  (16, 16),
+               },
+        widget=ImageWidget(
+            label='Image',
+            label_msgid='schema_image_label',
+            i18n_domain='EasyShop',
+        ),
+        storage=AttributeStorage()
+    ),    
+),
+)
+
+schema = ATCTMixin.schema.copy() + schema.copy()
+schema["title"].required = False
 
 class ShippingMethod(OrderedBaseFolder):
     """A simple shipping method.
