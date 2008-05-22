5ab5bcb52f25165f1a169c6dac56c3c25417355d
kai.diefenbach <kai.diefenbach@localhost>
2008-05-22 10:05:57 +0000

    Show parent and child categories
    svn path=/easyshop.search/trunk/; revision=1109

--- easyshop.search/easyshop/search/portlets/search_results.py
+++ easyshop.search/easyshop/search/portlets/search_results.py
@@ -11,6 +11,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 # easyshop imports
 from easyshop.core.config import _
+from easyshop.core.interfaces import ICategory
 from easyshop.core.interfaces import ICategoryManagement
 
 class ISearchResultsPortlet(IPortletDataProvider):
@@ -54,25 +55,58 @@ class Renderer(base.Renderer):
         
         category_amounts = {}
         category_titles = {}
+        category_levels = {}
+        category_tops = {}
+        
         for product in products:
             cm = ICategoryManagement(product)
+
             for category in cm.getTopLevelCategories():
-                uid = category.UID()
-                if category_amounts.has_key(uid) == False:
-                    category_amounts[uid] = 0
-                    category_titles[uid] = category.Title()
+
+                # Traverse to top category
+                
+                object = category
+                while ICategory.providedBy(object) == True:
+                    uid = object.UID()
+                    category_titles[uid] = object.Title()
+                    category_levels[uid] = len(object.getPhysicalPath())
                     
-                category_amounts[uid] += 1
+                    if category_amounts.has_key(uid) == False:
+                        category_amounts[uid] = 0
+                    
+                    category_amounts[uid] += 1
+                    
+                    temp = object
+                    object = object.aq_inner.aq_parent
+                    
+                    if ICategory.providedBy(object) == False:
+                        category_tops[uid] = temp
                 
         
         result = []
-        for uid in category_amounts.keys():
+        for uid, category in category_tops.items():
+
+            # Calculate children
+            children = []
+            for child in category.objectValues("Category"):
+                child_uid = child.UID()
+                if child_uid in category_titles.keys():
+                    children.append({
+                        "uid"    : child_uid,
+                        "title"  : category_titles[child_uid],
+                        "amount" : category_amounts[child_uid],
+                        "level"  : category_levels[child_uid]
+                    })
+                    
             result.append({
-                "uid"    : uid,
-                "title"  : category_titles[uid],
-                "amount" : category_amounts[uid],
-            }) 
-            
+                "uid"      : uid,
+                "title"    : category_titles[uid],
+                "amount"   : category_amounts[uid],
+                "level"    : category_levels[uid],
+                "children" : children
+            })
+        
+        import pdb; pdb.set_trace()
         return result
 
 class AddForm(base.NullAddForm):
