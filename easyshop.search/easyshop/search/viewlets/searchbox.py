bb20b9efb7e736622145cd93150094077c6f1207
kai.diefenbach <kai.diefenbach@localhost>
2008-05-28 20:57:45 +0000

    Using easyshop_path
    svn path=/easyshop.search/trunk/; revision=1145

--- easyshop.search/easyshop/search/viewlets/searchbox.py
+++ easyshop.search/easyshop/search/viewlets/searchbox.py
@@ -1,7 +1,19 @@
+# plone imports
 from plone.app.layout.viewlets.common import SearchBoxViewlet as Base
+
+# Five imports
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+# CMFCore imports
+from Products.CMFCore.utils import getToolByName
 
 class SearchBoxViewlet(Base):
     """
     """
     render = ViewPageTemplateFile('searchbox.pt')
+    
+    def getShopUrl(self):
+        """
+        """
+        ptool = getToolByName(self.context, "portal_properties")
+        return ptool.site_properties.easyshop_path
