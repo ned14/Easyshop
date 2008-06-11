721ed5e987a5ad4c77c03c192726cf007da4fa97
kai.diefenbach <kai.diefenbach@localhost>
2008-06-11 16:54:38 +0000

    Changed handling of asynchron payment methods. Displaying thank you page now.
    svn path=/easyshop.order/trunk/; revision=1159

--- easyshop.order/easyshop/order/browser/mail_order_received.py
+++ easyshop.order/easyshop/order/browser/mail_order_received.py
@@ -1,18 +1,21 @@
 # easyshop imports
 from easyshop.order.browser.order_view import OrderView
+from easyshop.core.interfaces import IPaymentInformationManagement
 
 class MailOrderReceivedView(OrderView):
     """
     """ 
     def getNote(self):
+        """Returns the note from the selected payment method.
         """
-        """
-        payment_data = self.getSelectedPaymentData()
-        payment_method = payment_data.get("payment_method")
+        customer = self.context.getCustomer()
+        pm = IPaymentInformationManagement(customer)
         
-        note = payment_method.getNote()
+        selected_payment_method = pm.getSelectedPaymentMethod()
+
+        note =  selected_payment_method.getNote()
         
         payment_url = self.context.absolute_url() + "/pay"
-        note = note.replace("[paypal-link]", payment_url)
+        note = note.replace("[payment-url]", payment_url)
         
         return note
