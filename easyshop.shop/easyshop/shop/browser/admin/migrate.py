cc90b8d767483089727c31ed2a0e6ae4524b0ad2
kai.diefenbach <kai.diefenbach@localhost>
2008-06-26 18:01:57 +0000

    svn path=/easyshop.shop/trunk/; revision=1174

--- easyshop.shop/easyshop/shop/browser/admin/migrate.py
+++ easyshop.shop/easyshop/shop/browser/admin/migrate.py
@@ -1,8 +1,13 @@
+import transaction
+
 # Five imports
 from Products.Five.browser import BrowserView
 
 # CMFCore imports
 from Products.CMFCore.utils import getToolByName
+
+# easyshop imports
+from easyshop.core.interfaces import ICategory
 
 class MigrateView(BrowserView):
     """
@@ -10,13 +15,43 @@ class MigrateView(BrowserView):
     def migrate(self):
         """
         """
-        # self.context._initBTrees()
-        # self.context.reindexObject()
-        
         utool = getToolByName(self.context, "portal_url")
         portal = utool.getPortalObject()
+        shop = portal.shop2
         
-        shop = portal["shop"]
+        catalog = getToolByName(self.context, "portal_catalog")
+        brains = catalog.searchResults(
+            portal_type = "Category",
+        )
         
-        import pdb; pdb.set_trace()
-        self.context._populateFromFolder(shop)
+        for brain in brains:
+                
+            category = brain.getObject()
+            
+            parent = category.aq_inner.aq_parent
+            if ICategory.providedBy(parent):
+                category.setParentCategory(parent)
+                category.setPositionInParent(category.getObjPositionInParent())
+                
+        transaction.commit()
+        
+        for brain in brains:
+            category = brain.getObject()
+
+            if len(category.getPhysicalPath()) == 5:
+                ids = category.objectIds("Category")
+                if len(ids) > 0:
+                    data = category.manage_cutObjects(ids)
+                    shop.manage_pasteObjects(data)
+
+        transaction.commit()
+        
+        for brain in brains:
+            category = brain.getObject()
+
+            if len(category.getPhysicalPath()) == 4:
+                ids = category.objectIds("Category")
+                if len(ids) > 0:
+                    data = category.manage_cutObjects(ids)
+                    shop.manage_pasteObjects(data)
+                    
