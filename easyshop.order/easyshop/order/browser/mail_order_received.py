19d9fee84d7116906042af465ec6d8670abe5aa6
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2008-07-29 16:06:31 +0000

    Tax without suffix
    svn path=/easyshop.order/trunk/; revision=1214

--- easyshop.order/easyshop/order/browser/mail_order_received.py
+++ easyshop.order/easyshop/order/browser/mail_order_received.py
@@ -19,3 +19,13 @@ class MailOrderReceivedView(OrderView):
         note = note.replace("[payment-url]", payment_url)
         
         return note
+        
+    def getPriceForCustomer(self):
+        """
+        """
+        p = IPrices(self.context)        
+        price = p.getPriceForCustomer()
+
+        cm = ICurrencyManagement(self.context)
+        return cm.priceToString(price, suffix=None)
+        
