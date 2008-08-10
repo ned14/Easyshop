eae4a99c223f0109cfbed530de35847308571331
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2008-08-10 13:20:57 +0000

    First working version to remove path.
    svn path=/easyshop.management/trunk/; revision=1259

--- easyshop.management/easyshop/management/browser/manage_redirections.py
+++ easyshop.management/easyshop/management/browser/manage_redirections.py
@@ -23,3 +23,15 @@ class ManageRedirectionsView(BrowserView):
             })
             
         return result
+        
+    def removePath(self):
+        """
+        """
+        path = self.request.get("path", "")
+        
+        if path != "" and storage.hasPath(path):
+            storage = getUtility(IRedirectionStorage)
+            storage.remove(path)
+        
+        url = self.context.absolute_url() + "/manage-redirections"
+        self.request.response.redirect(url)
