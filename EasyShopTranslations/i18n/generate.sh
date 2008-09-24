1bced1cbc3f55c9a3897cf4610c628139821a48e
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2008-09-24 14:32:52 +0000

    Updated translations
    svn path=/EasyShopTranslations/trunk/; revision=1423

--- EasyShopTranslations/i18n/generate.sh
+++ EasyShopTranslations/i18n/generate.sh
@@ -9,13 +9,12 @@ i18ndude rebuild-pot --pot easyshop.pot --create EasyShop --merge extra.pot\
     ../../../src/easyshop.groups\
     ../../../src/easyshop.information\
     ../../../src/easyshop.io\
-    ../../../src/easyshop.management\    
-    ../../../src/easyshop.marketing\    
     ../../../src/easyshop.order\
     ../../../src/easyshop.payment\
     ../../../src/easyshop.shipping\
-    ../../../src/easyshop.shop\
     ../../../src/easyshop.stocks\
+    ../../../src/easyshop.shop
+
                                     
 i18ndude sync --pot easyshop.pot easyshop-de.po
 i18ndude sync --pot easyshop-schema.pot easyshop-schema-de.po
