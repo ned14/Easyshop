3b481f5f67bfbb6520dd85108aa9c14fda826005
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2008-09-26 22:38:15 +0000

    Removed parent url
    svn path=/easyshop.order/trunk/; revision=1456

--- easyshop.order/easyshop/order/browser/order_view.py
+++ easyshop.order/easyshop/order/browser/order_view.py
@@ -1,3 +1,5 @@
+from AccessControl import Unauthorized
+
 # zope imports
 from zope.interface import Interface
 from zope.interface import implements
@@ -139,11 +141,6 @@ class OrderView(BrowserView):
             else:
                 url = product.absolute_url()
             
-            if IProductVariant.providedBy(product):
-                parent_url = product.aq_inner.aq_parent
-            else:
-                parent_url = None
-                    
             # Properties 
             for property in item.getProperties():
                 if IProductVariant.providedBy(product) == True:
@@ -155,7 +152,6 @@ class OrderView(BrowserView):
                 "product_title"        : item.getProductTitle(),
                 "product_quantity"     : item.getProductQuantity(),
                 "product_url"          : url,
-                "parent_url"           : parent_url,
                 "product_price_gross"  : product_price_gross,
                 "price_gross"          : price_gross,
                 "tax_rate"             : tax_rate,
