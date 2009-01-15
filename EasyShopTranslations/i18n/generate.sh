bec08f3e0b247344b2f7ff9dda3f01e2ddf42b56
danielle.davout <danielle.davout@localhost>
2009-01-15 00:35:18 +0000

    update the list of folders to be checked to build the pot
    svn path=/EasyShopTranslations/trunk/; revision=1519

--- EasyShopTranslations/i18n/generate.sh
+++ EasyShopTranslations/i18n/generate.sh
@@ -5,17 +5,23 @@ i18ndude rebuild-pot --pot easyshop.pot --create EasyShop --merge extra.pot\
     ../../../src/easyshop.catalog\
     ../../../src/easyshop.checkout\
     ../../../src/easyshop.core\
+    ../../../src/easyshop.criteria\
     ../../../src/easyshop.customers\
+    ../../../src/easyshop.discounts\
     ../../../src/easyshop.groups\
     ../../../src/easyshop.information\
     ../../../src/easyshop.io\
+    ../../../src/easyshop.login\
+    ../../../src/easyshop.kss\
+    ../../../src/easyshop.management\
     ../../../src/easyshop.order\
     ../../../src/easyshop.payment\
     ../../../src/easyshop.shipping\
     ../../../src/easyshop.stocks\
-    ../../../src/easyshop.shop
+    ../../../src/easyshop.shop\
+    ../../../src/easyshop.taxes\
 
-                                    
+
 i18ndude sync --pot easyshop.pot easyshop-de.po
 i18ndude sync --pot easyshop-schema.pot easyshop-schema-de.po
 i18ndude sync --pot easyshop-plone.pot easyshop-plone-de.po
