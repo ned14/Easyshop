cd239463eeb255a57c7569c23903cc555390ffce
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2009-02-16 08:27:57 +0000

    Typo
    svn path=/easyshop.catalog/; revision=1534

--- easyshop.catalog/branches-memcached/easyshop/catalog/__init__.py
+++
@@ -1,6 +0,0 @@
-from Products.CMFCore import DirectoryView
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
-    # Register skin directory
-    DirectoryView.registerDirectory('skins', globals())
--- easyshop.catalog/branches-memcached/easyshop/catalog/content/__init__.py
+++
@@ -1,8 +0,0 @@
-import category
-import containers
-import easyshop_image
-import product
-import product_property
-import product_property_option
-import product_selector
-import product_variant
