35f271d748b6396f50c5e0631d2b96fa73411edb
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2009-04-22 06:26:22 +0000

    svn path=/easyshop.shop/trunk/; revision=1553

--- easyshop.shop/easyshop/shop/browser/admin/migrate.py
+++ easyshop.shop/easyshop/shop/browser/admin/migrate.py
@@ -8,13 +8,45 @@ from Products.CMFCore.utils import getToolByName
 
 # easyshop imports
 from easyshop.core.interfaces import ICategory
+from easyshop.core.interfaces import IPrices
 
 class MigrateView(BrowserView):
     """
     """
+    # def migrate(self):
+    #     """
+    #     """
+    #     catalog = getToolByName(self.context, "portal_catalog")
+    #     brains = catalog.searchResults(
+    #         portal_type = "ProductVariant",
+    #     )
+    #     
+    #     result = ""
+    #     for brain in brains:
+    #         product = brain.getObject()
+    #         
+    #         if product.getForSale():
+    #             price_1 = product.getSalePrice()            
+    #         else:
+    #             price_1 = product.getPrice()            
+    #         price_2 = IPrices(product).getPriceGross()
+    #         
+    #         if price_1 != price_2:
+    #             result += "%s %s %s\n" % (product.Title(), price_1, price_2)
+    #         
+    #     return result
+
     def migrate(self):
         """
         """
-        import pdb; pdb.set_trace()
-        from Products.CacheSetup.enabler import enableCacheFu
-        enableCacheFu(self, True)
+        catalog = getToolByName(self.context, "portal_catalog")
+        brains = catalog.searchResults(
+            portal_type = "Product",
+        )
+        
+        result = ""
+        for brain in brains:
+            product = brain.getObject()
+            
+            if hasattr(product, "cache"):
+                product.cache.clear()
