c3f70af3c62aa33a419990782a74caee3109318e
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@localhost>
2009-07-09 11:08:57 +0000

    take only closed orders
    svn path=/easyshop.marketing/trunk/; revision=1561

--- easyshop.marketing/easyshop/marketing/browser/send_rating_mails.py
+++ easyshop.marketing/easyshop/marketing/browser/send_rating_mails.py
@@ -25,70 +25,75 @@ class SendRatingMailsView(BrowserView):
         """
         """
         live = self.request.get("live")
-        
+
         now = DateTime()
-        
+
         # Get sender
         mail_addresses = IMailAddresses(self.context)
         sender         = mail_addresses.getSender()
         bcc            = ["kai.diefenbach@iqpp.de"]
-        
+
         wftool = getToolByName(self.context, "portal_workflow")
-        
+
         # Get all relevant orders
         om = IOrderManagement(self.context)
-        
+
         for order in om.getOrders():
-            
+
             # Send mail only once (below "rating-mail" is set)
             if "rating-mail" in order.getMarketingInfo():
                 continue
 
-            # Send mail only for closed orders
-            if wftool.getInfoFor(order, "review_state") != "closed":
-                continue
-                            
-            # Send only mails for orders which are older than 2 weeks
-            # difference in days
-            if now - order.created() < (2*7):
-                continue
+            try:    
+                last_state = wftool.getInfoFor(order, "review_history")[-1]
+            except IndexError:
+                continue                
             
-            # Send only mails for orders which have at least on product with 
+            # Send mail only for closed orders
+            if last_state["review_state"] != "closed":
+                continue
+
+            # Send only mails for orders which closed at lease2 weeks
+            # ago
+            if now - last_state["time"] < (2*7):
+                continue
+
+            # Send only mails for orders which have at least on product with
             # a valid url (aka product which is not deleted in the meanwhile)
             if self.hasItems(order) == False:
                 continue
-            
+
             if live is None:
-                receiver = "usenet@diefenba.ch, d.kommol@demmelhuber.net"
+                receiver = "usenet@diefenba.ch"
                 if sender and receiver:
                     view = getMultiAdapter((order, order.REQUEST), name="rating-mail")
                     text = view()
-                                
+
                     # get charset
                     props = getToolByName(order, "portal_properties").site_properties
                     charset = props.getProperty("default_charset")
-                
+
                     sendMultipartMail(
                         context  = order,
                         sender   = sender,
                         receiver = receiver,
                         subject  = "Bitte bewerten Sie Ihr Produkt",
                         text     = text,
-                        charset  = charset)                    
+                        charset  = charset)
             else:
                 # Get receiver
                 customer = order.getCustomer()
                 address = IAddressManagement(customer).getShippingAddress()
                 receiver = address.email
-            
+
                 if sender and receiver:
                     view = getMultiAdapter((order, order.REQUEST), name="rating-mail")
                     text = view()
-                                
+
                     # get charset
                     props = getToolByName(order, "portal_properties").site_properties
                     charset = props.getProperty("default_charset")
-                
+
                     sendMultipartMail(
                         context  = order,
                         sender   = sender,
@@ -97,12 +102,12 @@ class SendRatingMailsView(BrowserView):
                         subject  = "Bitte bewerten Sie Ihr Produkt",
                         text     = text,
                         charset  = charset)
-                    
+
                     # set marketing info
                     mi = list(order.getMarketingInfo())
                     mi.append("rating-mail")
                     order.setMarketingInfo(mi)
-                
+
     def hasItems(self, order):
         """Returns True if order has at least one item with valid url
         """
