2bc0f1ea144a4a6dbc94a02e384d14a6d74dd9d3
pedrotschki <pedrotschki@localhost>
2009-10-07 15:27:03 +0000

    unicode character support for country vocabulary
    svn path=/easyshop.core/trunk/; revision=1624

--- easyshop.core/easyshop/core/vocabularies.py
+++ easyshop.core/easyshop/core/vocabularies.py
@@ -1,4 +1,5 @@
 # zope imports
+from zope.component import queryUtility
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
@@ -7,16 +8,19 @@ from easyshop.core.interfaces import IShopManagement
 
 # CMFPlone imports
 from Products.CMFPlone.utils import safe_unicode
+from plone.i18n.normalizer.interfaces import IIDNormalizer
 
 def countries(context):
     """
     """
     terms = []
     # terms.append(SimpleTerm("Germany", u"Germany"))
-    # terms.append(SimpleTerm("USA", u"USA"))    
+    # terms.append(SimpleTerm("USA", u"USA"))
     # terms.append(SimpleTerm("Polen", u"Polen"))
-    
+
     for country in IShopManagement(context).getShop().getCountries():
-        terms.append(SimpleTerm(country, country))    
-        
+        country = safe_unicode(country)
+        term_value = queryUtility(IIDNormalizer).normalize(country)
+        terms.append(SimpleTerm(term_value, term_value, country))
+
     return SimpleVocabulary(terms)
