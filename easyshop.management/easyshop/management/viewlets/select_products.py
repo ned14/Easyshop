cd2432a41e0616178030fd0ab110eaf2e8ab5e64
pedrotschki <pedrotschki@localhost>
2009-10-08 13:30:05 +0000

    make product management catalog aware
    svn path=/easyshop.management/trunk/; revision=1641

--- easyshop.management/easyshop/management/viewlets/select_products.py
+++ easyshop.management/easyshop/management/viewlets/select_products.py
@@ -20,48 +20,35 @@ class SelectProductsViewlet(ViewletBase):
     """
     """
     render = ViewPageTemplateFile('select_products.pt')
-    
+
     def getProducts(self):
         """
         """
         if self.request.get("form-sent") is None:
             return []
-            
+
         query =  Eq("path", "/".join(self.context.getPhysicalPath()))
         query &= Eq("object_provides", "easyshop.core.interfaces.catalog.IProduct")
-        
+
         search_text = self.request.get("search_text", "")
         search_category = self.request.get("search_category", [])
-        
+
         if search_text != "":
             query &= Eq("Title", search_text)
-        
+
         if len(search_category) > 0:
             query &= In("categories", search_category)
 
         catalog = getToolByName(self.context, "portal_catalog")
         brains = catalog.evalAdvancedQuery(query)
-        
-        result = []
-        for brain in brains:
-            product = brain.getObject()
-            
-            categories = ", ".join([c.Title() for c in product.getCategories()])
-            
-            result.append({
-                "uid"   : product.UID(),
-                "title" : product.Title(),
-                "price" : product.getPrice(),
-                "categories" : categories
-            })
 
-        return result
-        
+        return brains
+
     def getSelectedCategories(self):
         """
         """
         selected_categories = self.request.get("search_category", [])
         if not isinstance(selected_categories, (list, tuple)):
             selected_categories = (selected_categories,)
-        
+
         return selected_categories
