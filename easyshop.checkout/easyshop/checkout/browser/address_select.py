eebbfbc55b7861d3d378da4df64f2e8326618dcc
pedrotschki <pedrotschki@localhost>
2009-10-14 13:44:50 +0000

    merged r1625-r1630 from asynchronous_payment_workflow branch. template cleanup
    svn path=/easyshop.checkout/trunk/; revision=1650

--- easyshop.checkout/easyshop/checkout/browser/address_select.py
+++ easyshop.checkout/easyshop/checkout/browser/address_select.py
@@ -12,7 +12,7 @@ from Products.Five.formlib import formbase
 # CMFPlone imports
 from Products.CMFPlone.utils import safe_unicode
 
-# easyshop.core imports 
+# easyshop.core imports
 from easyshop.core.config import _
 from easyshop.core.interfaces import IAddressManagement
 from easyshop.core.interfaces import ICheckoutManagement
@@ -25,27 +25,27 @@ class IAddressSelectForm(Interface):
     """
     invoice_address  = schema.TextLine()
     shipping_address = schema.TextLine()
-    
+
 class ShopAddressSelectForm:
     """
     """
     implements(IAddressSelectForm)
     adapts(IShop)
-    
+
     def __init__(self, context):
         """
         """
         self.context = context
-        
+
     invoice_address  = u""
     shipping_address = u""
 
 
-# Using here Five's EditForm by heart because plone.app.form would lock the shop 
+# Using here Five's EditForm by heart because plone.app.form would lock the shop
 # which is not desired in this case (because the shop is not modified at all)
 class AddressSelectForm(formbase.EditForm):
     """
-    """ 
+    """
     template = pagetemplatefile.ZopeTwoPageTemplateFile("address_select.pt")
     form_fields = form.Fields(IAddressSelectForm)
 
@@ -56,7 +56,7 @@ class AddressSelectForm(formbase.EditForm):
         customer = ICustomerManagement(self.context).getAuthenticatedCustomer()
         customer.selected_invoice_address  = data.get("invoice_address", "")
         customer.selected_shipping_address = data.get("shipping_address", "")
-        
+
         ICheckoutManagement(self.context).redirectToNextURL("SELECTED_ADDRESSES")
 
     @form.action(_(u"label_add_address", default="Add Address"), name=u'add_address')
@@ -66,10 +66,10 @@ class AddressSelectForm(formbase.EditForm):
         customer = ICustomerManagement(self.context).getAuthenticatedCustomer()
         customer_url = customer.absolute_url()
         template_url = self.context.absolute_url() + "/checkout-select-addresses"
-        
+
         url = customer_url + "/add-address?goto=" + template_url
         self.request.response.redirect(url)
-        
+
     def getShippingAddresses(self):
         """Returns all addresses with the currently selected invoice address
         checked.
@@ -90,20 +90,20 @@ class AddressSelectForm(formbase.EditForm):
 
             address_as_dict = self._getAddressAsDict(address)
             address_as_dict["checked"] = checked
-            
+
             line.append(address_as_dict)
-            
+
             if (index + 1) % 3 == 0:
                 result.append(line)
                 line = []
-                
+
         result.append(line)
-        
+
         if len(result) > 0 and found_selected_address == False:
             result[0][0]["checked"] = True
 
         return result
-        
+
     def getInvoiceAddresses(self):
         """
         """
@@ -123,15 +123,15 @@ class AddressSelectForm(formbase.EditForm):
 
             address_as_dict = self._getAddressAsDict(address)
             address_as_dict["checked"] = checked
-            
+
             line.append(address_as_dict)
-            
+
             if (index + 1) % 3 == 0:
                 result.append(line)
                 line = []
-                
+
         result.append(line)
-        
+
         if found_selected_address == False:
             result[0][0]["checked"] = True
 
@@ -149,6 +149,6 @@ class AddressSelectForm(formbase.EditForm):
             "address1"    : address.address_1,
             "zipcode"     : address.zip_code,
             "city"        : address.city,
-            "country"     : address.country,
+            "country"     : address.country_title(),
             "phone"       : address.phone,
             }
