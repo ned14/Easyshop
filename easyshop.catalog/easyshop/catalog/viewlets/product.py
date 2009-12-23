85feee15f8d980a5c27180a32cf16a58ff16a570
pedrotschki <pedrotschki@localhost>
2009-12-23 12:19:48 +0000

    fixed consideration of formatted option in customerTotalPrice
    svn path=/easyshop.catalog/trunk/; revision=1672

--- easyshop.catalog/easyshop/catalog/viewlets/product.py
+++ easyshop.catalog/easyshop/catalog/viewlets/product.py
@@ -5,7 +5,7 @@ from zope.viewlet.interfaces import IViewlet
 
 # CMFPlone imports
 from Products.CMFPlone.utils import tuplize
-        
+
 # plone imports
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.memoize.instance import memoize
@@ -47,35 +47,38 @@ class ProductViewlet(ViewletBase):
         """
         selected_accessories = self.request.get("accessories", [])
         selected_accessories = tuplize(selected_accessories)
-        
+
         product_price = self.getPriceForCustomer(formatted=False)
-        
+
         total_accessories = 0
         for accessory in self.getAccessories():
             # We take only selected accessories into account
             if accessory["uid"] in selected_accessories:
                 total_accessories += accessory["total_raw_price"]
-        
-        total_price = product_price + total_accessories    
-        
-        cm = ICurrencyManagement(self.context)
-        return cm.priceToString(total_price, suffix=None)
+
+        total_price = product_price + total_accessories
+
+        if formatted:
+            cm = ICurrencyManagement(self.context)
+            return cm.priceToString(total_price, suffix=None)
+        else:
+            return total_price
 
     def getPriceForCustomer(self, formatted=True):
         """
         """
         p = IPrices(self.context)
         price = p.getPriceForCustomer()
-        
+
         if IProductVariantsManagement(self.context).hasVariants() == False:
             total_diff = 0.0
             pm = IPropertyManagement(self.context)
             for property_id, selected_option in self.request.form.items():
-                if property_id.startswith("property"):                
+                if property_id.startswith("property"):
                     total_diff += pm.getPriceForCustomer(
                         property_id[42:],
                         selected_option
-                    )            
+                    )
             price += total_diff
 
         if formatted == True:
@@ -83,52 +86,52 @@ class ProductViewlet(ViewletBase):
             return cm.priceToString(price, suffix=None)
         else:
             return price
-    
+
     def getTotalStandardPriceForCustomer(self):
         """
         """
         selected_accessories = self.request.get("accessories", [])
         selected_accessories = tuplize(selected_accessories)
-        
+
         product_price = self.getStandardPriceForCustomer(formatted=False)
-        
+
         total_accessories = 0
         for accessory in self.getAccessories():
             # We take only selected accessories into account
             if accessory["uid"] in selected_accessories:
                 total_accessories += accessory["total_raw_standard_price"]
-        total_price = product_price + total_accessories    
-        
+        total_price = product_price + total_accessories
+
         cm = ICurrencyManagement(self.context)
         return cm.priceToString(total_price, suffix=None)
-        
+
     def getStandardPriceForCustomer(self, formatted=True):
-        """Returns the standard price for a customer when the product is for 
+        """Returns the standard price for a customer when the product is for
         sale. Used to display the crossed-out standard price.
         """
         p = IPrices(self.context)
         price = p.getPriceForCustomer(effective=False)
-        
+
         if IProductVariantsManagement(self.context).hasVariants() == False:
             total_diff = 0.0
             pm = IPropertyManagement(self.context)
             for property_id, selected_option in self.request.form.items():
-                if property_id.startswith("property"):                
+                if property_id.startswith("property"):
                     total_diff += pm.getPriceForCustomer(
                         property_id[42:],
                         selected_option
-                    )            
+                    )
             price + total_diff
-        
+
         if formatted == True:
             cm = ICurrencyManagement(self.context)
             return cm.priceToString(price, suffix = None)
         else:
             return price
-                            
+
     def getProductData(self):
         """
-        """        
+        """
         data = IData(self.context)
         return data.asDict()
 
@@ -142,45 +145,45 @@ class ProductViewlet(ViewletBase):
             result.append(
                 "%s/image_tile"  % image.absolute_url(),
             )
-                        
+
         return result
-    
+
     def getAccessories(self):
         """
         """
         catalog = getToolByName(self.context, "portal_catalog")
-        
+
         accessories = tuplize(self.request.get("accessories", []))
 
         result = []
         for uid_with_quantiy in self.context.getAccessories():
             uid, quantity = uid_with_quantiy.split(":")
-                        
+
             try:
-                brain = catalog.searchResults(UID=uid)[0]                
+                brain = catalog.searchResults(UID=uid)[0]
             except IndexError:
-                # This could happend if the user doesn't have the View 
+                # This could happend if the user doesn't have the View
                 # permission (e.g. product is private)
                 continue
-                
+
             product = brain.getObject()
-            
+
             # Try to get quantity out of request (In case the customer has
             # changed them within the form). If there is none we take the default
             # quantity which are given from the shop owner within the accessories
             # admin interface.
             quantity = self.request.get("%s_quantity" % product.UID(), quantity)
-            
-            # Same viewlet with the context of the accessory to get the 
-            # properties of the accessory.            
+
+            # Same viewlet with the context of the accessory to get the
+            # properties of the accessory.
             viewlet = getMultiAdapter((product, self.request, self.view, self.manager), IViewlet, name="easyshop.product-viewlet")
             properties = viewlet.getProperties()
-            
+
             try:
                 quantity = int(quantity)
             except ValueError:
                 quantity = 1
-                
+
             # Standard price
             standard_price = viewlet.getStandardPriceForCustomer(formatted=False)
             total_raw_standard_price = quantity * standard_price
@@ -191,65 +194,65 @@ class ProductViewlet(ViewletBase):
             # Effective price
             price = viewlet.getPriceForCustomer(formatted=False)
             total_raw_price = quantity * price
-            
+
             cm = ICurrencyManagement(self.context)
             price = cm.priceToString(price, suffix=None)
             total_price = cm.priceToString(total_raw_price, suffix=None)
-            
+
             result.append({
                 "uid" : uid,
                 "title" : brain.Title,
                 "quantity" : quantity,
                 "checked" : uid in accessories,
                 "for_sale" : product.getForSale(),
-                "total_raw_price" : total_raw_price, 
-                "total_raw_standard_price" : total_raw_standard_price, 
+                "total_raw_price" : total_raw_price,
+                "total_raw_standard_price" : total_raw_standard_price,
                 "standard_price" : standard_price,
                 "total_standard_price" : total_standard_price,
                 "price" : price,
                 "total_price" : total_price,
                 "properties" : properties,
             })
-        
+
         return result
-            
-    
+
+
     def getProperties(self):
         """
-        """    
+        """
         pvm = IProductVariantsManagement(self.context)
         if pvm.hasVariants():
             return self._getPropertiesForVariants()
         else:
             return self._getPropertiesForConfiguration()
-            
+
     def _getPropertiesForConfiguration(self):
         """
         """
         u = queryUtility(INumberConverter)
         cm = ICurrencyManagement(self.context)
-                        
+
         selected_options = {}
         for name, value in self.request.items():
             if name.startswith("property"):
                 selected_options[name[42:]] = value
-        
+
         pm = IPropertyManagement(self.context)
-        
+
         result = []
         for property in pm.getProperties():
-            
+
             # Only properties with at least one option are displayed.
             if len(property.getOptions()) == 0:
                 continue
-            
+
             # Preset with select option
             options = [{
                 "id"       : "select",
                 "title"    : _(u"Select"),
                 "selected" : False,
             }]
-            
+
             for option in property.getOptions():
 
                 # generate value string
@@ -263,17 +266,17 @@ class ProductViewlet(ViewletBase):
                     content = "%s %s" % (option_name, option_price)
                 else:
                     content = option_name
-                        
+
                 # is option selected?
                 selected_option = selected_options.get(property.getId(), "")
                 selected = option_id == selected_option
-                
+
                 options.append({
                     "id"       : option_id,
                     "title"    : content,
                     "selected" : selected,
                 })
-                
+
             result.append({
                 "id"      : "property_%s_%s" % (self.context.UID(), property.getId()),
                 "title"   : property.Title(),
@@ -281,13 +284,13 @@ class ProductViewlet(ViewletBase):
             })
 
         return result
-        
+
     def _getPropertiesForVariants(self):
         """
         """
         u = queryUtility(INumberConverter)
         cm = ICurrencyManagement(self.context)
-        
+
         selected_options = {}
         for name, value in self.request.items():
             if name.startswith("property"):
@@ -295,7 +298,7 @@ class ProductViewlet(ViewletBase):
                     selected_options[name[42:]] = value
                 else:
                     selected_options[name[9:]] = value
-                
+
         # If nothing is selected we select the default variant
         if selected_options == {}:
             pvm = IProductVariantsManagement(self.context)
@@ -308,16 +311,16 @@ class ProductViewlet(ViewletBase):
             for property in default_variant.getForProperties():
                 name, value = property.split(":")
                 selected_options[name] = value
-            
+
         pm = IPropertyManagement(self.context)
-        
+
         result = []
         for property in pm.getProperties():
-            
+
             # Only properties with at least one option are displayed.
             if len(property.getOptions()) == 0:
                 continue
-            
+
             options = []
             for option in property.getOptions():
 
@@ -329,17 +332,17 @@ class ProductViewlet(ViewletBase):
                 # option_price = cm.priceToString(option_price, "long", "after")
                 # content = "%s %s" % (option_name, option_price)
                 content = option_name
-                        
+
                 # is option selected?
                 selected_option = selected_options.get(property.getId(), "")
                 selected = option_id == selected_option
-                
+
                 options.append({
                     "id"       : option_id,
                     "title"    : content,
                     "selected" : selected,
                 })
-                
+
             result.append({
                 "id"      : "property_%s_%s" % (self.context.UID(), property.getId()),
                 "title"   : property.Title(),
@@ -347,7 +350,7 @@ class ProductViewlet(ViewletBase):
             })
 
         return result
-        
+
     def getRelatedProducts(self):
         """
         """
@@ -358,65 +361,65 @@ class ProductViewlet(ViewletBase):
         for product in self.context.getRefs('products_products'):
             if mtool.checkPermission("View", product) is not None:
                 result.append(product)
-            
+
         return result
 
     def getStockInformation(self):
         """
-        """        
+        """
         shop = self._getShop()
         sm = IStockManagement(shop)
-        
+
         pvm = IProductVariantsManagement(self.context)
-        
+
         if pvm.hasVariants() == False:
-            stock_information = sm.getStockInformationFor(self.context)            
+            stock_information = sm.getStockInformationFor(self.context)
         else:
             product_variant = pvm.getSelectedVariant()
 
             # First, we try to get information for the selected product variant
             stock_information = sm.getStockInformationFor(product_variant)
 
-            # If nothing is found, we try to get information for parent product 
+            # If nothing is found, we try to get information for parent product
             # variants object.
             if stock_information is None:
                 stock_information = sm.getStockInformationFor(self.context)
-            
+
         if stock_information is None:
             return None
-            
+
         return IData(stock_information).asDict()
-                    
+
     def showAddQuantity(self):
         """
         """
-        shop = self._getShop() 
+        shop = self._getShop()
         return shop.getShowAddQuantity()
-    
+
     def getShopURL(self):
         """
         """
-        return IShopManagement(self.context).getShop().absolute_url()  
+        return IShopManagement(self.context).getShop().absolute_url()
 
 
     def getBreadCrumbs(self) :
         """
         """
         categories = self.context.getCategories()
-        parents = [] 
-         
+        parents = []
+
         if len(categories) > 0 :
-          
-          category  = categories[0]         
-          
+
+          category  = categories[0]
+
           while category is not None:
             parents.append({"title"         : category.Title(),
                             "absolute_url"  : category.absolute_url()})
-           
-            category = category.getParentCategory() 
-        
+
+            category = category.getParentCategory()
+
         parents.reverse()
-        
+
         return parents
 
 
