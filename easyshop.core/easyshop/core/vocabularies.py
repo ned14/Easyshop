e123b1f2441128e26bcbfcdff350e90c9fe49b79
pedrotschki <pedrotschki@localhost>
2010-02-25 18:15:20 +0000

    fixed issue in add/edit customer addresses. country vocabulary could't determine shop when no context was given. fixed this by searching portal_catalog for EasyShop instances. this may lead to wrong country data if more than one EasyShop instance is installed
    svn path=/easyshop.core/trunk/; revision=1692

--- easyshop.core/easyshop/core/vocabularies.py
+++ easyshop.core/easyshop/core/vocabularies.py
@@ -1,5 +1,6 @@
 # zope imports
 from zope.component import queryUtility
+from zope.component import getSiteManager
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
@@ -18,7 +19,26 @@ def countries(context):
     # terms.append(SimpleTerm("USA", u"USA"))
     # terms.append(SimpleTerm("Polen", u"Polen"))
 
-    for country in IShopManagement(context).getShop().getCountries():
+    shop = IShopManagement(context).getShop()
+
+    if shop is None:
+        # BBB: this is ugly because if we create a new address
+        #      we dont have any context. try to guess the first EasyShop
+        #      in portal_catalog and hope we are in luck!
+        # XXX: this breaks if the site has more than one EasyShop instances!!!
+        sm = getSiteManager()
+        cat_query = sm.portal_catalog.queryCatalog(
+            dict(portal_type='EasyShop'))
+
+        if len(cat_query)<1:
+            # this is not possible, because we weren't in this vocab if
+            # we hadn't an EasyShop instance
+            __traceback_info__ = "no Easyshop found in portal_catalog"
+            raise Exception
+
+        shop = cat_query[0].getObject()
+
+    for country in shop.getCountries():
         country = safe_unicode(country)
         term_value = queryUtility(IIDNormalizer).normalize(country)
         terms.append(SimpleTerm(term_value, term_value, country))
